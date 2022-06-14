51c2d85d06b3b0fb3ba48c5d2870407c409b6f77
Richard Hess <eswat2@gmail.com>
2022-06-14 10:57:25 -0700

    refactor solution test

--- tests/solution.js
+++ tests/solution.js
@@ -4,18 +4,18 @@ import { dataSet } from '../utils/mocks.js'
 
 describe('solution', function () {
   this.timeout(10000)
-  const id = 42
+  const id = 342
   const keys = ['id', 'data', 'summary']
   const tags = ['makes', 'vins', 'counts']
 
-  it(`should be an object... ${keys}`, function (done) {
+  it(`should be an object... ${keys} & ${id}`, function (done) {
     const callback = (data) => {
       const solution = dataSet(data, id)
       try {
         expect(solution)
           .to.be.a('object')
           .that.contains.all.keys(...keys)
-        expect(solution.id).to.equal(42)
+        expect(solution.id).to.equal(id)
         expect(solution.data).to.be.a('object').that.contains.all.keys('dealers')
         expect(solution.summary)
           .to.be.a('object')
