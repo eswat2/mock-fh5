09f575015b0e9673234b45f2fad233f71d796dfb
Richard Hess <eswat2@gmail.com>
2022-06-14 12:35:53 -0700

    expand tests

--- tests/solution.js
+++ tests/solution.js
@@ -2,48 +2,46 @@ import { expect } from 'chai'
 import { fetchData } from '../utils/fh5-data.js'
 import { dataSet } from '../utils/mocks.js'
 
+const keys = ['id', 'data', 'summary']
+const tags = ['makes', 'vins', 'counts']
+const sums = ['dealers', 'makes', 'vehicles']
+
+const checkSolution = (solution, id) => {
+  expect(solution)
+    .to.be.a('object')
+    .that.contains.all.keys(...keys)
+  expect(solution.id).to.equal(id)
+  console.log('-- solution.id:', solution.id)
+  expect(solution.data).to.be.a('object').that.contains.all.keys('dealers')
+  expect(solution.summary)
+    .to.be.a('object')
+    .that.contains.all.keys(...tags)
+  expect(solution.summary.counts)
+    .to.be.a('object')
+    .that.contains.all.keys(...sums)
+  console.log('-- counts:', solution.summary.counts)
+}
+
+const fetchSolution = (done, id) => {
+  const callback = (data) => {
+    const solution = id ? dataSet(data, id) : dataSet(data)
+    try {
+      checkSolution(solution, id ? id : 42)
+      done()
+    } catch (error) {
+      done(error)
+    }
+  }
+  fetchData(callback)
+}
+
 describe('solution', () => {
   const id = 342
-  const keys = ['id', 'data', 'summary']
-  const tags = ['makes', 'vins', 'counts']
-
-  it(`should return an object... ${keys} & 42`, (done) => {
-    const callback = (data) => {
-      const solution = dataSet(data)
-      try {
-        expect(solution)
-          .to.be.a('object')
-          .that.contains.all.keys(...keys)
-        expect(solution.id).to.equal(42)
-        expect(solution.data).to.be.a('object').that.contains.all.keys('dealers')
-        expect(solution.summary)
-          .to.be.a('object')
-          .that.contains.all.keys(...tags)
-        done()
-      } catch (error) {
-        done(error)
-      }
-    }
-    fetchData(callback)
+  it(`should return an object... ${keys}`, (done) => {
+    fetchSolution(done)
   })
 
   it(`should return an object... ${keys} & ${id}`, (done) => {
-    const callback = (data) => {
-      const solution = dataSet(data, id)
-      try {
-        expect(solution)
-          .to.be.a('object')
-          .that.contains.all.keys(...keys)
-        expect(solution.id).to.equal(id)
-        expect(solution.data).to.be.a('object').that.contains.all.keys('dealers')
-        expect(solution.summary)
-          .to.be.a('object')
-          .that.contains.all.keys(...tags)
-        done()
-      } catch (error) {
-        done(error)
-      }
-    }
-    fetchData(callback)
+    fetchSolution(done, id)
   })
 })
